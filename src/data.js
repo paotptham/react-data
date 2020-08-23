1240c35311821de7bee240f2b88f3b7e63594ad6
koppol16 <koppol_tey@hotmail.com>
2020-08-23 12:10:20 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export default {
+products : [
+{id:'1001' , name:'เสื้อเชิ้ต' , price:200, stock:10},
+{id:'1002' , name:'กางเกง' , price:1200, stock:50},
+{id:'1003' , name:'รองเท้า' , price:500, stock:100}
+
+]
+
+
+}
